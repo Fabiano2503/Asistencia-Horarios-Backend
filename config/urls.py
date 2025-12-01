00e26c229eb3bc2412b05b3e81983a5a8ea567dd
MilaGrosLey <yashminramos@gmail.com>
2025-12-01 17:28:12 -0500

    Add reports API URL to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/practicantes/', include('apps.practicantes.infrastructure.urls')),
     path('api/bot/', include('apps.bot_discord.infrastructure.urls')),
+    path("api/v1/reports/", include("apps.reporte.infrastructure.urls", namespace="reporte")),
+
 ]
