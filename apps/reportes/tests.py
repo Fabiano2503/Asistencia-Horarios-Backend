f72a4da274e59a57708096fe89ad0a96aab6258c
MilaGrosLey <yashminramos@gmail.com>
2025-12-02 00:19:25 -0500

    Update tests.py

--- apps/reportes/tests.py
+++ apps/reportes/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from apps.practicantes.views import ...
+
 
 
 class ReportesAPITests(APITestCase):
