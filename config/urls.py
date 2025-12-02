c45c83d8608d4bac2ea2d7625d5cc42ad27581e4
MilaGrosLey <yashminramos@gmail.com>
2025-12-01 23:22:59 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('api/practicantes/', include('apps.practicantes.infrastructure.urls')),
     path('api/bot/', include('apps.bot_discord.infrastructure.urls')),
 
-    # Nueva app puntualidad
-    path('api/puntualidad/', include('apps.puntualidad.urls')),
-
     # Nueva app reportes
     path('api/v1/reports/', include('apps.reporte.infrastructure.urls', namespace='reporte')),
 ]
