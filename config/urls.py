8f43d96ede69e9c7b31a29bcc573f9388190117c
MilaGrosLey <yashminramos@gmail.com>
2025-12-01 23:19:35 -0500

    Update URL patterns and clean up comments
    Removed unnecessary comments and added new URL patterns for the punctualidad and reportes apps.

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,16 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Apps existentes
     path('api/practicantes/', include('apps.practicantes.infrastructure.urls')),
     path('api/bot/', include('apps.bot_discord.infrastructure.urls')),
 
+    # Nueva app puntualidad (rama main)
+    path('api/puntualidad/', include('apps.puntualidad.urls')),
+
+    # Nueva app reportes (rama feature/reportes)
+    path('api/v1/reports/', include('apps.reporte.infrastructure.urls', namespace='reporte')),
 ]
