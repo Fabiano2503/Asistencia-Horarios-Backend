a649c36e53ef52ea08e13555d36c1464247faed6
MilaGrosLey <yashminramos@gmail.com>
2025-12-01 23:28:09 -0500

    Update report routes and add punctuality path

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Apps existentes
     path('api/practicantes/', include('apps.practicantes.infrastructure.urls')),
     path('api/bot/', include('apps.bot_discord.infrastructure.urls')),
 
-    # Nueva app reportespath("api/v1/reports/", include("apps.reporte.infrastructure.urls")),
-    path("api/v1/reports/", include("apps.reporte.infrastructure.urls")),
+    # Rutas de reportes (carpeta apps/reportes)
+    path('api/v1/reports/', include('apps.reportes.infrastructure.urls')),
 
+    path('api/puntualidad/', include('apps.puntualidad.urls')),
 ]
